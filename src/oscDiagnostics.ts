517915d94f57bdfab9dd40e347d4213a0bcae082
Christoph Hausner <christoph_hausner@hotmail.de>
2018-08-27 22:03:56 +0200

    add diagnostic: check that name and types in component declarations match their sections

--- src/oscDiagnostics.ts
+++ src/oscDiagnostics.ts
@@ -13,7 +13,8 @@ export class OSCDiagnostics {
 		this.checkComponentTypesAndFieldsExist(document, diagnostics);
 		await this.checkRequiredFieldsArePresent(document, diagnostics);
 		await this.warnForBackwardCompatibilityFlags(document, diagnostics);
-
+		await this.checkDeclarationsAndSections(document, diagnostics);
+		
 		diagnosticCollection.set(document.uri, diagnostics);
 	}
 
@@ -90,6 +91,51 @@ export class OSCDiagnostics {
 						}
 						let diagnosticMessage = `Field "${fieldInfo.field}" of ${component.componentType} instance "${component.instanceName}" should be set to "${fieldInfo.recommendedValue}" to disable backward-compatibility behavior.`;
 						diagnostics.push(new Diagnostic(diagnosticRange, diagnosticMessage, vscode.DiagnosticSeverity.Warning));
+					}
+				}
+			}
+		}
+	}
+
+	private static async checkDeclarationsAndSections(document: TextDocument, diagnostics: Diagnostic[]): Promise<void> {
+		let components = await configParser.parse(document);
+		let componentManager = components.find(c => c.instanceName === 'componentInstances' && c.componentType === 'cComponentManager');
+		// check whether every component has a declaration and the component types match
+		for (let component of components) {
+			if (component.componentType !== 'cComponentManager') {
+				let fieldValue = undefined;
+				if (componentManager !== undefined) {
+					// we explicitly check that the assignment exists before calling getFieldValue because getFieldValue currently does not handle fields in associative arrays properly
+					if (componentManager.fieldAssignments.find(assignment => assignment.field === `instance[${component.instanceName}].type`) !== undefined) {
+						fieldValue = componentManager.getFieldValue(`instance[${component.instanceName}].type`);
+					}
+				}
+				if (fieldValue !== undefined) {
+					if (fieldValue.value !== component.componentType) {
+						let diagnosticRange = component.sectionHeaders[0].componentTypeRange;
+						let diagnosticMessage = `Component "${component.instanceName}" was declared with type ${fieldValue.value} but type ${component.componentType} was specified in the component section header.`;
+						diagnostics.push(new Diagnostic(diagnosticRange, diagnosticMessage, vscode.DiagnosticSeverity.Error));
+					}
+				} else {
+					let diagnosticRange = component.sectionHeaders[0].componentInstanceRange;
+					let diagnosticMessage = `Declaration of component "${component.instanceName}" is missing. This component will be ignored by openSMILE.`;
+					diagnostics.push(new Diagnostic(diagnosticRange, diagnosticMessage, vscode.DiagnosticSeverity.Warning));
+				}
+			}
+		}
+		// check whether component sections exist for each declared component
+		if (componentManager) {
+			for (let assignment of componentManager.fieldAssignments) {
+				let match = /^instance\[(.+)\]\.type$/.exec(assignment.field); 
+				if (match) {
+					let instanceName = match[1];
+					let componentType = assignment.value;
+					let component = components.find(c => c.instanceName === instanceName && c.componentType === componentType);
+					// we give no error for dataMemory:cDataMemory as this component usually never has a section
+					if (!component && !(instanceName === 'dataMemory' && componentType === 'cDataMemory')) {
+						let diagnosticRange = new vscode.Range(assignment.fieldRange.start.translate(0, 9), assignment.fieldRange.end.translate(0, -6));
+						let diagnosticMessage = `Component section missing for declared component "${instanceName}".`;
+						diagnostics.push(new Diagnostic(diagnosticRange, diagnosticMessage, vscode.DiagnosticSeverity.Error));
 					}
 				}
 			}
